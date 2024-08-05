3b8a401f08657513f6c5fbe08c5e20f00b00852d
Steven_Medinets <Steven.w.medinets.com>
2024-08-04 20:49:34 -0400

    word count

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     print(text)
-
+    count = word_count(text)
+    print(f"{count} words found in the document")
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def word_count(path):
+    words = path.split()
+    return len(words)
 
 main()
