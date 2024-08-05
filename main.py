431f5cb7e386feb88d1bf1bc3eb10ffe92091837
Steven_Medinets <Steven.w.medinets.com>
2024-08-04 21:17:27 -0400

    sorted by alphabet...

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    #print(text)
     count = word_count(text)
-    print(f"{count} words found in the document")
+    #print(f"{count} words found in the document")
     chars_dict = get_chars_dict(text)
     print(chars_dict)
+    chars_list = list(chars_dict.items())
+    print(chars_list)
+    chars_list.sort()
+    print(chars_list)
 
 def get_book_text(path):
     with open(path) as f:
@@ -17,12 +21,15 @@ def word_count(path):
 
 def get_chars_dict(text):
     chars = {}  #create a directory
-    for c in text: #search each character in the directory
+    for c in text: #search each character in the text
         lowered = c.lower()
-        if lowered in chars:
+        if lowered.isalpha() == False:
+            pass
+        elif lowered in chars:
             chars[lowered] += 1 
         else:
             chars[lowered] = 1
     return chars
+
 
 main()
