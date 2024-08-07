be3d9c4402b2a7b773c14a8966538384fa317a32
Steven_Medinets <Steven.w.medinets.com>
2024-08-07 19:16:50 -0400

    reports on the book's character count

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 def main():
     book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    #print(text)
-    count = word_count(text)
-    #print(f"{count} words found in the document")
+    text = get_book_text(book_path)  #print(text)
+    
+    count = word_count(text) #print(f"{count} words found in the document")
     chars_dict = get_chars_dict(text)
+    sorted_list = count_sort(chars_dict)
 
-    chars_list = list(chars_dict.items())
-    chars_list.sort() #sorts alphabetically
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{count} words found in the document")
+    print()
 
-    print(chars_list)
+    for item in sorted_list:
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
 
 def get_book_text(path):
     with open(path) as f:
@@ -31,5 +36,14 @@ def get_chars_dict(text):
             chars[lowered] = 1
     return chars
 
+def sort_on(dict):
+    return dict["num"]
+
+def count_sort(chars_dict):
+    sorted_list = []
+    for c in chars_dict:
+        sorted_list.append({"char": c, "num": chars_dict[c]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 main()
